f662608e7b563abf1f3c48646afe742a271090ec
Nick <nickabrahams5544@gmail.com>
2025-11-24 22:02:52 +0100

    eerste commit

---
+++ index.py
@@ -0,0 +1,36 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+
+sky_surface = pygame.image.load('graphics/sky.png').convert_alpha()
+ground_surface = pygame.image.load('graphics/ground.png').convert_alpha()
+text_surface = test_font.render('my game', False, ('Black'))
+
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(bottomright = (600, 300))
+
+player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80,300))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    screen.blit(sky_surface, (0,0))
+    screen.blit(ground_surface,(0,300))
+    screen.blit(text_surface,(300,50))
+
+    snail_rect.x -= 4
+    if snail_rect.x <= -100:
+        snail_rect.left = 800
+    screen.blit(snail_surf, snail_rect)
+    screen.blit(player_surf,player_rect)
+
+    pygame.display.update()
+    clock.tick(60)
