f3674e4c5289dd38b6635353ae99812c5c87d69b
Nick <nickabrahams5544@gmail.com>
2025-11-26 11:14:46 +0100

    start scherm begin

--- index.py
+++ index.py
@@ -12,14 +12,11 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
-game_active = True
+game_active = False
 start_time = 0
 
 sky_surface = pygame.image.load('graphics/sky.png').convert_alpha()
 ground_surface = pygame.image.load('graphics/ground.png').convert_alpha()
-
-# score_surf = test_font.render('my game', False, ('Black'))
-# score_rect = score_surf.get_rect(center = (400,50))
 
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(bottomright = (800, 300))
@@ -27,7 +24,17 @@ snail_rect = snail_surf.get_rect(bottomright = (800, 300))
 player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80,300))
 player_gravity = 0
+
+# intro screen
 player_stand = pygame.image.load('graphics/player/player_stand.png').convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand,0,2)
+player_stand_rect = player_stand.get_rect(center = (400,200))
+
+game_name = test_font.render('Runner game',False,(111,196,169))
+game_name_rect = game_name.get_rect(center = (400,80))
+
+game_message = test_font.render('Press space to run',False,(111,196,169))
+game_message_rect = game_message.get_rect(center = (400,320))
 
 while True:
     for event in pygame.event.get():
@@ -69,8 +76,10 @@ while True:
             if snail_rect.colliderect(player_rect):
                 game_active = False
     else:
-            screen.fill((94,129,162))
-            # snail_rect.left = 800
+        screen.fill((94,129,162))
+        screen.blit(player_stand,player_stand_rect)
+        screen.blit(game_name,game_name_rect)
+        screen.blit(game_message,game_message_rect)
 
 
     pygame.display.update()
